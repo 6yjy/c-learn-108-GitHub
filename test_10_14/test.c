a251d82f0a1e891e9442abbead616cb8aef8bfc5
yi-jiya <3491240020@qq.com>
2022-10-14 10:57:19 +0800

    C代码(10-23练习)

---
+++ test_10_14/test.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+//10-23练习
+
+/*
+//KiKi学会了printf在屏幕输出信息，他想输出一架小飞机。请帮他编写程序输出这架小飞机
+#include <stdio.h>
+
+int main(void)
+{
+    printf("     **     \n");
+    printf("     **     \n");
+    printf("************\n");
+    printf("************\n");
+    printf("    *  *    \n");
+    printf("    *  *    \n");
+
+    return 0;
+
+	return 0;
+}
+*/
+
+/*
+//输入一个整数表示一个人的智商，如果大于等于140，则表明他是一个天才，输出“Genius”
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0;
+    while (scanf("%d", &a) != EOF)
+    {
+        if (a >= 140)
+        {
+            printf("Genius\n");
+        }
+    }
+    return 0;
+
+    return 0;
+}
+*/
+
+/*
+//你的手机丢了，在屏幕上输出信息告诉大家
+#include <stdio.h>
+
+int main(void)
+{
+    printf("I lost my cellphone!\n");
+
+    return 0;
+}
+*/
+
+/*
+//请计算表达式“(-8+22)×a-10+c÷2”，其中，a = 40，c = 212
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 40;
+    int c = 212;
+    int ret = (-8 + 22) * a - 10 + c / 2;
+    printf("%d\n", ret);
+    //printf("%d\n", (-8+22)*a-10+c/2);
+
+    return 0;
+}
+*/
