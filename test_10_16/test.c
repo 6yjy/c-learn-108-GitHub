2011b55d64e91ce8f132ec7aadb0db125c889a87
yi-jiya <3491240020@qq.com>
2022-10-16 14:37:41 +0800

    C代码(10-16练习)

---
+++ test_10_16/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+/*
+//strlen = 7
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    printf("%d\n", strlen("c:\test\121"));
+
+    return 0;
+}
+*/
+
+/*
+//局部变量优先
+#include <stdio.h>
+int num = 10;
+int main()
+{
+    int num = 1;
+    printf("num = %d\n", num);
+
+    return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char arr[] = { 'b', 'i', 't' };
+    printf("%d\n", strlen(arr));
+
+    return 0;
+}
+*/
